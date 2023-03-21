f2f9552c5f4c1a9cc2e9d88a64a58b6f2b9e6c8b
Marcus Puchalla <marcus.puchalla@tastenkunst.io>
2023-03-21 12:47:57 +0100

    feat: Remove strict mode that causes double contructor
    Note: I'm not a react developer but this strict mode causing double
    initialization of the meerket object, which messes up the wallet
    discovery. We might need to change this again if this breaks
    something.

--- demo-wallet-app/src/index.tsx
+++ demo-wallet-app/src/index.tsx
@@ -7,9 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
